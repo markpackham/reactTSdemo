c34d35dffc3b2581e54e0f9a82d4583bef6334ee
mark <markpackham1@gmail.com>
2022-09-29 17:19:52 +0100

    output options
    https://youtu.be/bAJlYgeovlg?t=1219

--- src/Select.tsx
+++ src/Select.tsx
@@ -19,15 +19,26 @@ export type SelectOption = {
   
   type SelectProps = {
     options: SelectOption[]
-  } & (SingleSelectProps | MultipleSelectProps)
+    value?: SelectOption
+    onChange: (value: SelectOption | undefined) => void
+  } 
 
 export function Select({value, onChange, options}:SelectProps){
     return (
-        <div className={styles.container}>
-        <span className={styles.value}>Value</span>
+        <div tabIndex={0} className={styles.container}>
+        <span className={styles.value}></span>
         <button className={styles["clear-btn"]}>&times;</button>
         <div className={styles.divider}></div>
         <div className={styles.caret}></div>
+        
+        <ul className={`${styles.options} ${styles.show}`}>
+        {options.map(option => (
+          <li key={option.label} className={styles.option}>
+            {option.label}
+          </li>
+        ))}
+      </ul>
+
         </div>
     )
 }
