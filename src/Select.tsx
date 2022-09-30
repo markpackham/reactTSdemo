d42c278151495622469f7095a4835879076d9cc2
mark <markpackham1@gmail.com>
2022-09-30 07:22:39 +0100

    have code handle single and multi select
    https://youtu.be/bAJlYgeovlg?t=2000

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,86 +1,95 @@
-import { useEffect, useRef, useState } from "react"
-import styles from "./select.module.css"
+import { useEffect, useRef, useState } from "react";
+import styles from "./select.module.css";
 
 export type SelectOption = {
-    label: string
-    value: string | number
-  }
-    
-  type MultipleSelectProps = {
-    multiple: true
-    value: SelectOption[]
-    onChange: (value: SelectOption[]) => void
-  }
-  
-  type SingleSelectProps = {
-    multiple?: false
-    value?: SelectOption
-    onChange: (value: SelectOption | undefined) => void
-  }
-  
-  type SelectProps = {
-    options: SelectOption[]
-  } & (SingleSelectProps | MultipleSelectProps)
+  label: string;
+  value: string | number;
+};
 
-export function Select({multiple, value, onChange, options}:SelectProps){
-  const [isOpen, setIsOpen] = useState(false)
-  const [highlightedIndex, setHighlightedIndex] = useState(0)
+type MultipleSelectProps = {
+  multiple: true;
+  value: SelectOption[];
+  onChange: (value: SelectOption[]) => void;
+};
 
-  function clearOptions(){
-    onChange(undefined)
+type SingleSelectProps = {
+  multiple?: false;
+  value?: SelectOption;
+  onChange: (value: SelectOption | undefined) => void;
+};
+
+type SelectProps = {
+  options: SelectOption[];
+} & (SingleSelectProps | MultipleSelectProps);
+
+export function Select({ multiple, value, onChange, options }: SelectProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [highlightedIndex, setHighlightedIndex] = useState(0);
+
+  function clearOptions() {
+    multiple ? onChange([]) : onChange(undefined);
   }
 
-  function selectOption(option: SelectOption){
-    if(option !== value){
-      onChange(option)
+  function selectOption(option: SelectOption) {
+    if (multiple) {
+      if (value.includes(option)) {
+        onChange(value.filter((o) => o !== option));
+      } else {
+        onChange([...value, option]);
+      }
+    } else {
+      if (option !== value) {
+        onChange(option);
+      }
     }
   }
 
-  function isOptionSelected(option: SelectOption){
-    return option === value
+  function isOptionSelected(option: SelectOption) {
+    return multiple ? value.includes(option) : option === value;
   }
 
-  useEffect(()=>{
-    if(isOpen) setHighlightedIndex(0)
-  },[isOpen])
+  useEffect(() => {
+    if (isOpen) setHighlightedIndex(0);
+  }, [isOpen]);
 
-    return (
-        <div
-        onBlur={()=> setIsOpen(false)}
-        onClick={()=> setIsOpen(prev => !prev)}
-        tabIndex={0} 
-        className={styles.container}
-        >
-        <span className={styles.value}>{value?.label}</span>
-        <button 
-        onClick={(e =>{
-          e.stopPropagation()
-          clearOptions()
-        })} 
-        className={styles["clear-btn"]}>&times;</button>
-        <div className={styles.divider}></div>
-        <div className={styles.caret}></div>
-        
-        <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
+  return (
+    <div
+      onBlur={() => setIsOpen(false)}
+      onClick={() => setIsOpen((prev) => !prev)}
+      tabIndex={0}
+      className={styles.container}
+    >
+      <span className={styles.value}>{value?.label}</span>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          clearOptions();
+        }}
+        className={styles["clear-btn"]}
+      >
+        &times;
+      </button>
+      <div className={styles.divider}></div>
+      <div className={styles.caret}></div>
+
+      <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
         {options.map((option, index) => (
-
-          <li 
-          onClick={e =>{
-            e.stopPropagation()
-            selectOption(option)
-            setIsOpen(false)
-          }}
-          onMouseEnter={() => setHighlightedIndex(index)}
-          key={option.value} 
-          className={`${styles.option} ${
-            isOptionSelected(option) ? styles.selected : ""
-          } ${index === highlightedIndex ? styles.highlighted : ""}`}>
+          <li
+            onClick={(e) => {
+              e.stopPropagation();
+              selectOption(option);
+              setIsOpen(false);
+            }}
+            onMouseEnter={() => setHighlightedIndex(index)}
+            key={option.value}
+            className={`${styles.option} ${
+              isOptionSelected(option) ? styles.selected : ""
+            } ${index === highlightedIndex ? styles.highlighted : ""}`}
+          >
             {option.label}
           </li>
-
         ))}
       </ul>
-
-        </div>
-    )
+    </div>
+  );
 }
